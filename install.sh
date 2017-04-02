628533216d5cd9b967cc01aa366d2928c259e44e
Cameron Jones <deimyts@gmail.com>
2017-04-02 14:49:10 -0500

    symlink dotfiles into home dir

--- install.sh
+++ install.sh
@@ -1,2 +1,17 @@
 #!/usr/bin/env bash
 
+# Get current dir (so you can run this script from anywhere)
+export DOTFILES_DIR
+DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Symlink dotfiles into home directory
+# -s: Create a symlink instead of a new file.
+# -f: If the target file already exists, then unlink it so that the link may occur.
+# -v: verbose: show files as they are processed.
+
+ln -sfv "$DOTFILES_DIR/git/.gitconfig" ~
+ln -sfv "$DOTFILES_DIR/git/.gitignore_global" ~
+ln -sfv "$DOTFILES_DIR/zsh/.zshrc" ~
+ln -sfv "$DOTFILES_DIR/tmux/.tmux.conf" ~
+ln -sfv "$DOTFILES_DIR/vim/.vimrc" ~
+
