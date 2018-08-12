e4b6927e289e8b7ae19783bac5369fe7dfaa321d
Cameron Jones <deimyts@gmail.com>
2018-08-12 16:25:42 -0500

    feat(tmux): update tmux-conf for linux

--- install.sh
+++ install.sh
@@ -4,6 +4,9 @@
 export DOTFILES_DIR
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# create a directory for get repos
+mkdir ~/repos
+
 # Symlink dotfiles into home directory
 # -s: Create a symlink instead of a new file.
 # -f: If the target file already exists, then unlink it so that the link may occur.
@@ -14,8 +17,16 @@ ln -sfv "$DOTFILES_DIR/git/.gitconfig" ~
 ln -sfv "$DOTFILES_DIR/git/.gitignore_global" ~
 ln -sfv "$DOTFILES_DIR/tmux/.tmux.conf" ~
 # ln -sfv "$DOTFILES_DIR/vim/.vimrc" ~
-mkdir -p ~/.config/nvim;
-ln -sfv "$DOTFILES_DIR/vim/.vimrc" ~/.config/nvim/init.vim;
+mkdir -p ~/.config/nvim
+ln -sfv "$DOTFILES_DIR/vim/.vimrc" ~/.config/nvim/init.vim
 
-chsh -s $(which zsh);
+chsh -s $(which zsh)
 ln -sfv "$DOTFILES_DIR/zsh/.zshrc" ~
+
+# Install plugin repos
+# zsh
+git clone https://github.com/bhilburn/powerlevel9k.git ~/repos/powerlevel9k
+
+# tmux
+mkdir -p ~/.tmux/plugins
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
