192ab5354af784b6f84cd9e56be722579c08353b
Cameron Jones <deimyts@gmail.com>
2018-08-12 15:52:08 -0500

    feat(install): add install script for linux mint

---
+++ linux-mint.install.sh
@@ -0,0 +1,28 @@
+#! /usr/bin/env bash
+
+# sudo apt-get update;
+# apt-get upgrade;
+# apt-get install git;
+
+sudo apt-get install neovim;
+sudo apt-get install tmux;
+sudo apt-get install zsh;
+# curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash;
+export NVM_DIR="$HOME/.nvm";
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh";  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion";  # This loads nvm bash_completion
+nvm install node;
+
+#setup powerlevel9k and install fonts
+git clone https://github.com/bhilburn/powerlevel9k.git ~/repos/powerlevel9k;
+echo 'source  ~/repos/powerlevel9k/powerlevel9k.zsh-theme' >> ~/.zshrc
+
+wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
+wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
+
+mkdir -p ~/.local/share/fonts
+mkdir -p ~/.config/fontconfig/conf.d
+
+mv PowerlineSymbols.otf ~/.local/share/fonts/
+fc-cache -vf ~/.local/share/fonts/
+mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
